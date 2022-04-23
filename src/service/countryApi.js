8403065422a9015dbf0264fcd6d4f4e1166f0443
Rupesh <rupeshghosh10@gmail.com>
2022-04-23 21:52:37 +0530

    replaced axios with fetch api

--- src/service/countryApi.js
+++ src/service/countryApi.js
@@ -1,6 +1,5 @@
-import axios from 'axios';
-
 export const getAllCountries = async () => {
-  const req = await axios.get('https://restcountries.com/v3.1/all');
-  return req.data;
+  const res = await fetch('https://restcountries.com/v3.1/all');
+  const data = await res.json();
+  return data;
 }
